a58a56d048735c342ec44c2802d7cdfea210a4ce
njanke96 <njanke96@uvic.ca>
2021-10-14 22:42:39 -0700

    file menu and sample shortcut

--- Scripts/Ui/MainWindow.cs
+++ Scripts/Ui/MainWindow.cs
@@ -1,21 +1,30 @@
 using Godot;
-using System;
 
 namespace CSC473.Scripts.Ui
 {
 	public class MainWindow : Panel
 	{
+		// Menu item ids
+		enum FileMenuItem
+		{
+			Load,
+			Save,
+			Quit
+		}
+		
 		private StateManager _stateManager;
-		private Label _statusLabel;
 		private Viewport _viewport3d;
+
+		private PopupMenu _fileMenu;
 		
 		public override void _Ready()
 		{
 			// get node refs
 			_viewport3d = GetNode<Viewport>("OuterMargin/MainContainer/VPSidebar" + 
 													 "/ViewportContainer/Viewport");
-			_statusLabel = GetNode<Label>("OuterMargin/MainContainer/StatusContainer/Label");
-			
+
+			_fileMenu = GetNode<MenuButton>("OuterMargin/MainContainer/MenuButtons/File").GetPopup();
+
 			// preload 3d viewport scene (could use ResourceInteractiveLoader in future)
 			Node root3d = ResourceLoader.Load<PackedScene>("res://3DView.tscn").Instance();
 			_viewport3d.AddChild(root3d);
@@ -27,6 +36,19 @@ namespace CSC473.Scripts.Ui
 			mSensSlider.Connect("value_changed", this, nameof(_MouseSensChanged));
 			GetNode<Label>("OuterMargin/MainContainer/VPSidebar/SideBar/LMouseSens")
 				.Text = "Mouse Sensitivity: " + mSensSlider.Value;
+			
+			// populate menubar
+			_fileMenu.AddItem("Load Layout", (int)FileMenuItem.Load);
+			_fileMenu.AddItem("Save Layout", (int)FileMenuItem.Save);
+			_fileMenu.AddSeparator();
+			_fileMenu.AddItem("Quit", (int)FileMenuItem.Quit);
+			
+			// shortcuts
+			ShortCut ctrls = new ShortCut();
+			InputEvent ctrlsAction = (InputEvent)InputMap.GetActionList("ui_save")[0];
+			ctrls.Shortcut = ctrlsAction;
+			int ctrlsIndex = _fileMenu.GetItemIndex((int) FileMenuItem.Save);
+			_fileMenu.SetItemShortcut(ctrlsIndex, ctrls);
 		}
 		
 		public void _MouseSensChanged(float value)
