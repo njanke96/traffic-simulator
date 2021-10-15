32e27c21815fac67fbc9531907d87494bf88adfa
njanke96 <njanke96@uvic.ca>
2021-10-14 21:32:10 -0700

    Move mouse sensitivity setting logic to main window

--- Scripts/Ui/MainWindow.cs
+++ Scripts/Ui/MainWindow.cs
@@ -5,6 +5,7 @@ namespace CSC473.Scripts.Ui
 {
 	public class MainWindow : Panel
 	{
+		private StateManager _stateManager;
 		private Label _statusLabel;
 		private Viewport _viewport3d;
 		
@@ -18,6 +19,24 @@ namespace CSC473.Scripts.Ui
 			// preload 3d viewport scene (could use ResourceInteractiveLoader in future)
 			Node root3d = ResourceLoader.Load<PackedScene>("res://3DView.tscn").Instance();
 			_viewport3d.AddChild(root3d);
+			
+			_stateManager = GetNode<StateManager>("/root/StateManager");
+			
+			// mouse sensitivity settings
+			Slider mSensSlider = GetNode<Slider>("OuterMargin/MainContainer/VPSidebar/SideBar/MouseSens");
+			mSensSlider.Connect("value_changed", this, nameof(_MouseSensChanged));
+			GetNode<Label>("OuterMargin/MainContainer/VPSidebar/SideBar/LMouseSens")
+				.Text = "Mouse Sensitivity: " + mSensSlider.Value;
+		}
+		
+		public void _MouseSensChanged(float value)
+		{
+			// updated mouse sens to the state manager
+			_stateManager.SMouseSensitivity = value;
+			
+			// update label
+			GetNode<Label>("OuterMargin/MainContainer/VPSidebar/SideBar/LMouseSens")
+				.Text = "Mouse Sensitivity: " + value;
 		}
 	}
 }
