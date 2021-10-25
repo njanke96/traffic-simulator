c1d99b71a219773a32210652cf346ba7dc577056
njanke96 <njanke96@uvic.ca>
2021-10-24 22:44:44 -0700

    drivey cars

---
+++ Scripts/HumanVehicleController.cs
@@ -0,0 +1,43 @@
+﻿using Godot;
+
+namespace CSC473.Scripts
+{
+    /// <summary>
+    /// Human controller for testing
+    /// </summary>
+    public class HumanVehicleController : Node
+    {
+        private VehicleBody _vehicle;
+
+        public override void _Ready()
+        {
+            _vehicle = GetParent<VehicleBody>();
+        }
+
+        public override void _PhysicsProcess(float delta)
+        {
+            // the vehicle may not be ready yet
+            if (_vehicle == null)
+                return;
+            
+            // 1.0 is full accel, -1.0 is full brake
+            float accel = Input.GetActionStrength("drive_forward") - Input.GetActionStrength("drive_backward");
+
+            // 1.0 is full left, -1.0 is full right
+            float steer = Input.GetActionStrength("drive_left") - Input.GetActionStrength("drive_right");
+
+            if (accel > 0.0)
+            {
+                _vehicle.EngineForce = 40f * accel;
+                _vehicle.Brake = 0f;
+            }
+            else
+            {
+                _vehicle.EngineForce = 0f;
+                _vehicle.Brake = -1f * accel;
+            }
+
+            _vehicle.Steering = 0.5f * steer;
+        }
+    }
+}
---
+++ Scripts/Vehicle.cs
@@ -0,0 +1,163 @@
+﻿using Godot;
+
+namespace CSC473.Scripts
+{
+    /// <summary>
+    /// Base class for vehicles.
+    /// </summary>
+    public class Vehicle : VehicleBody
+    {
+        // body color
+        private Color _color;
+
+        public Color BodyColor
+        {
+            get => _color;
+            set
+            {
+                _color = value;
+                SpatialMaterial material = new SpatialMaterial();
+                material.AlbedoColor = value;
+                _body.SetSurfaceMaterial(_colorMaterialIndex, material);
+            }
+        }
+
+        // rigidbody properties
+
+        // body mesh
+        private MeshInstance _body;
+
+        // path to model
+        private readonly string _modelPath;
+        private readonly string _collisionShapePath;
+        private readonly int _colorMaterialIndex;
+
+        // wheel attributes
+        private readonly float _wheelRadius;
+        private readonly float _suspTravel;
+        private readonly float _strutLen;
+
+        /// <summary>
+        /// The default constructor is intended for debugging, when attaching this script to a VehicleBody
+        /// node in the editor this constructor is used.
+        /// </summary>
+        public Vehicle()
+        {
+            _modelPath = "res://Assets/Models/Vehicles/sedan.glb";
+            _collisionShapePath = "res://Assets/Vehicles/SedanCollisionShape.tscn";
+            _colorMaterialIndex = 1;
+            _wheelRadius = 0.3f;
+            _suspTravel = 2.0f;
+            _strutLen = 0.1f;
+        }
+
+        /// <summary>
+        /// Initialize the vehicle.
+        /// </summary>
+        /// <param name="modelPath">Path to the model. Must be one of kenney's low poly cars in glb format.</param>
+        /// <param name="collisionShapePath">Path to collision shape scene.</param>
+        /// <param name="colorMaterialIndex">Material slot index for the main color material.</param>
+        /// <param name="wheelRadius">Radius of the wheel.</param>
+        /// <param name="suspTravel">Total suspension travel</param>
+        /// <param name="strutLen">Total strut length.</param>
+        public Vehicle(string modelPath, string collisionShapePath, int colorMaterialIndex = 1,
+            float wheelRadius = 0.3f, float suspTravel = 2.0f, float strutLen = 0.1f)
+        {
+            _modelPath = modelPath;
+            _collisionShapePath = collisionShapePath;
+            _colorMaterialIndex = colorMaterialIndex;
+            _wheelRadius = wheelRadius;
+            _suspTravel = suspTravel;
+            _strutLen = strutLen;
+        }
+
+        public override void _Ready()
+        {
+            Spatial mesh =
+                ResourceLoader.Load<PackedScene>(_modelPath).Instance<Spatial>();
+
+            CollisionShape cs =
+                ResourceLoader.Load<PackedScene>(_collisionShapePath)
+                    .Instance<CollisionShape>();
+
+            // vehicle body (for color changing)
+            _body = (MeshInstance) mesh.FindNode("body");
+            
+            // rotate vehicle 180 because vehicles forward vector must be +z
+            mesh.GetNode<Spatial>("tmpParent").RotateY(Mathf.Pi);
+
+            // rigidbody attributes
+            PhysicsMaterialOverride = new PhysicsMaterial();
+            PhysicsMaterialOverride.Friction = 0.5f;
+            
+
+            // get static wheel meshes
+            // wheels are flipped because of the rotation
+            MeshInstance wheelFrontLeft = (MeshInstance) mesh.FindNode("wheel_backLeft");
+            MeshInstance wheelFrontRight = (MeshInstance) mesh.FindNode("wheel_backRight");
+            MeshInstance wheelBackLeft = (MeshInstance) mesh.FindNode("wheel_frontLeft");
+            MeshInstance wheelBackRight = (MeshInstance) mesh.FindNode("wheel_frontRight");
+
+            // remove them from their parent
+            Node wheelParent = wheelBackLeft.GetParent();
+            wheelParent.RemoveChild(wheelBackLeft);
+            wheelParent.RemoveChild(wheelBackRight);
+            wheelParent.RemoveChild(wheelFrontLeft);
+            wheelParent.RemoveChild(wheelFrontRight);
+
+            // make them children of VehicleWheels
+            VehicleWheel vwBackLeft = new VehicleWheel();
+            VehicleWheel vwBackRight = new VehicleWheel();
+            VehicleWheel vwFrontLeft = new VehicleWheel();
+            VehicleWheel vwFrontRight = new VehicleWheel();
+
+            // The VehicleWheel nodes need to have the transforms of the vehicle wheel meshes, and the vehicle
+            // wheel meshes must be at <0, 0, 0> in the VehicleWheel coordinate system.
+            vwBackLeft.Transform = new Transform(Basis.Identity, new Vector3(wheelBackLeft.Transform.origin));
+            vwBackRight.Transform = new Transform(Basis.Identity, new Vector3(wheelBackRight.Transform.origin));
+            vwFrontLeft.Transform = new Transform(Basis.Identity, new Vector3(wheelFrontLeft.Transform.origin));
+            vwFrontRight.Transform = new Transform(Basis.Identity, new Vector3(wheelFrontRight.Transform.origin));
+            
+            // keep old rotations (bases)
+            wheelBackLeft.Transform = new Transform(wheelBackLeft.Transform.basis, Vector3.Zero);
+            wheelBackRight.Transform = new Transform(wheelBackRight.Transform.basis, Vector3.Zero);
+            wheelFrontLeft.Transform = new Transform(wheelFrontLeft.Transform.basis, Vector3.Zero);
+            wheelFrontRight.Transform = new Transform(wheelFrontRight.Transform.basis, Vector3.Zero);
+            
+            vwBackLeft.AddChild(wheelBackLeft);
+            vwBackRight.AddChild(wheelBackRight);
+            vwFrontLeft.AddChild(wheelFrontLeft);
+            vwFrontRight.AddChild(wheelFrontRight);
+            
+            // vehicles are front wheel drive
+            vwFrontLeft.UseAsSteering = true;
+            vwFrontRight.UseAsSteering = true;
+
+            // vehicles are subarus
+            vwBackLeft.UseAsTraction = true;
+            vwBackRight.UseAsTraction = true;
+            vwFrontLeft.UseAsTraction = true;
+            vwFrontRight.UseAsTraction = true;
+
+            // vehicle wheel attributes
+            VehicleWheel[] wheels = {vwBackLeft, vwBackRight, vwFrontLeft, vwFrontRight};
+            foreach (VehicleWheel whl in wheels)
+            {
+                whl.SuspensionTravel = _suspTravel;
+                whl.WheelRadius = _wheelRadius;
+                whl.WheelRestLength = _strutLen;
+                whl.SuspensionStiffness = 40f;
+            }
+
+            // add VehicleWheels as children of this VehicleBody
+            AddChild(vwBackLeft);
+            AddChild(vwBackRight);
+            AddChild(vwFrontLeft);
+            AddChild(vwFrontRight);
+
+            // finally add the mesh and collision shape as children
+            AddChild(mesh);
+            AddChild(cs);
+        }
+    }
+}
